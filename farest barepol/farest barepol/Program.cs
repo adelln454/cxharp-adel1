02097a8a2053d5046e834a773fcca82b2e8f2309
adelln454 <40113485+adelln454@users.noreply.github.com>
2018-06-09 15:41:45 +0300

    farst c charp
    my farst program

---
+++ farest barepol/farest barepol/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace farest_barepol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 25;
+            Console.WriteLine("my verbole valeom =(0)", x);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ farst program/farst program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace farst_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //i am good i am s1
+            Console.WriteLine("hello world");
+            Console.ReadKey();
+        }
+    }
+}
